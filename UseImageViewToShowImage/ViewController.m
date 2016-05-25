3ec0c91088a896da360d10f3838add635177161a
SJ <shijie7575@foxmail.com>
2016-05-25 15:46:55 +0800

    sj

--- UseImageViewToShowImage/ViewController.m
+++ UseImageViewToShowImage/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.Img.image = [UIImage imageNamed:@"8.jpg"];
 }
 
 - (void)didReceiveMemoryWarning {
